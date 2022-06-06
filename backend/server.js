58f39fc5a490432f9d2396d4d659261180aed609
Kevin Huang <kevinhuang451@gmail.com>
2022-06-06 18:56:36 -0400

    connect backend to frontend

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,38 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql2');
-const cors = require('cors');
+const cors = require('cors');//allow front end to communicate to backend
 const PORT = 3000;
 app.use(cors());
+//parse data from frontend to backend
 app.use(express.json());
 
+//connection to local database
 const db = mysql.createConnection({
     user: "root",
     host: "localhost",
     password: "kevin",
     database: "tasktracker"
 });
-
+//post request to insert into tasks database
 app.post("/addTask", (req, res) => {
     const task = req.body.task;
     const user = req.body.user;
-
+    //request information and then submit query in an array
     db.query("INSERT INTO tasks (task, user) VALUES (?, ?)",
         [task, user], (err, result) => {
             if (err) console.log(err);
-            else res.send("Values inserted successfully");
+            else res.send("Values Inserted Successfully");
         }
     );
 });
-
+//get request to retrieve data from tasks database
+app.get("/getTasks", (req, res) => {
+    db.query("SELECT * FROM tasks", (err, result) => {
+        if (err) console.log(err);
+        else res.send(result);
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
--- src/App.js
+++ src/App.js
@@ -8,23 +8,54 @@ function App() {
     const [task, setTask] = useState('');
     const [user, setUser] = useState('');
 
+    const [tasksList, setTaskList] = useState([]);
+
+    //addTask function to add data to database
     const addTask = () => {
         Axios.post("http://localhost:3000/addTask", {
             task: task,
             user: user
         }).then(() => {
-            console.log('success');
+            //add onto the existing array
+            setTaskList([
+                ...tasksList,
+                {
+                    task: task,
+                    user: user,
+                },
+
+            ]);
+        });
+    };
+    //get data from backend
+    const getTasks = () => {
+        Axios.get("http://localhost:3000/getTasks").then((response) => {
+            setTaskList(response.data);
         });
     };
 
+
     return (
         <div className="App">
-            <div className="information">
+            <div className="taskAssign">
                 <label>Task</label>
                 <input type="text" onChange={(event) => (setTask(event.target.value))} />
                 <label>Assigned To</label>
                 <input type="text" onChange={(event) => (setUser(event.target.value))} />
                 <button onClick={addTask}>Add Task</button>
+            </div>
+
+            <div className="showTasks">
+                <button onClick={getTasks}>Show Tasks</button>
+                {tasksList.map((val, key) => {
+                    return (
+                        <div className="users" key='{val.id}'>
+                            <h4>Task Assigned: {val.user}</h4>
+                            <h4>Task: { val.task }</h4>
+                            
+                        </div>
+                    );
+                })}
             </div>
         </div>
     );
