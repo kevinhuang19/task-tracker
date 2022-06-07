cb961662a7b2e478174571350d20663388e36cf9
Kevin Huang <kevinhuang451@gmail.com>
2022-06-06 20:01:47 -0400

    update and delete

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.post("/addTask", (req, res) => {
     const task = req.body.task;
     const user = req.body.user;
     //request information and then submit query in an array
-    db.query("INSERT INTO tasks (task, user) VALUES (?, ?)",
+    db.query("INSERT INTO tasks (task, user, entry_date) VALUES (?, ?, NOW())",
         [task, user], (err, result) => {
             if (err) console.log(err);
             else res.send("Values Inserted Successfully");
@@ -33,6 +33,26 @@ app.get("/getTasks", (req, res) => {
         else res.send(result);
     });
 });
+//put request to update a certain task
+app.put("/update", (req, res) => {
+    const id = req.body.id;
+    const task = req.body.task;
+    db.query("UPDATE tasks SET task = ? WHERE id = ?",
+        [task, id], (err, result) => {
+        if (err) console.log(err);
+        else res.send(result);
+    });
+
+})
+
+app.delete("/delete/:id", (req, res) => {
+    const id = req.params.id;
+    db.query("DELETE FROM tasks WHERE id = ?",
+        id, (err, result) => {
+            if (err) console.log(err);
+            else res.send(result);
+        });
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ import Axios from 'axios';
 function App() {
 
     const [task, setTask] = useState('');
-    const [user, setUser] = useState('');
+    const [user, setUser,entry_date] = useState('');
 
     const [tasksList, setTaskList] = useState([]);
+
+    const [newTask, setNewTask] = useState('');
 
     //addTask function to add data to database
     const addTask = () => {
         Axios.post("http://localhost:3000/addTask", {
             task: task,
-            user: user
+            user: user,
+            entry_date: entry_date,
         }).then(() => {
             //add onto the existing array
             setTaskList([
@@ -22,6 +25,7 @@ function App() {
                 {
                     task: task,
                     user: user,
+                    entry_date: entry_date
                 },
 
             ]);
@@ -33,15 +37,32 @@ function App() {
             setTaskList(response.data);
         });
     };
+    const updateTask = (id) => {
+        Axios.put("http://localhost:3000/update", {
+            task: newTask,
+            id: id
 
+        }).then((response) => {
+            setTaskList(tasksList.map((val) => {
+                return val.id === id ? {id: val.id, user: val.user, entry_date: val.entry_date, task: newTask}: val
+            }))
+        });
+    };
+    const deleteTask = (id) => {
+        Axios.delete(`http://localhost:3000/delete/${id}`).then((response) => {
+            setTaskList(tasksList.filter((val) => {
+                return val.id !== id;
+            }))
+        });
+    }
 
     return (
         <div className="App">
             <div className="taskAssign">
                 <label>Task</label>
-                <input type="text" onChange={(event) => (setTask(event.target.value))} />
+                <input type="text" onChange={(event) => (setTask(event.target.value))} required/>
                 <label>Assigned To</label>
-                <input type="text" onChange={(event) => (setUser(event.target.value))} />
+                <input type="text" onChange={(event) => (setUser(event.target.value))} required />
                 <button onClick={addTask}>Add Task</button>
             </div>
 
@@ -49,10 +70,17 @@ function App() {
                 <button onClick={getTasks}>Show Tasks</button>
                 {tasksList.map((val, key) => {
                     return (
-                        <div className="users" key='{val.id}'>
-                            <h4>Task Assigned: {val.user}</h4>
-                            <h4>Task: { val.task }</h4>
-                            
+                        <div className="users">
+                            <div>
+                                <h4>Task Assigned: {val.user}</h4>
+                                <h4>Task: {val.task}</h4>
+                                <h4>Date: {val.entry_date}</h4>
+                            </div>
+                            <div>
+                                <input type="text" placeholder="new task..." onChange={(event) => (setNewTask(event.target.value))} />
+                                <button onClick={() => { updateTask(val.id) }}>Update</button>
+                                <button onClick={() => {deleteTask(val.id) } }>Delete</button>
+                            </div>
                         </div>
                     );
                 })}
